c6c7adbb08f40d15b515d3ed169203b129520e90
enzo <enzoh321@gmail.com>
2020-02-06 16:46:36 +0100

    voila2

--- explo.c
+++ explo.c
@@ -5,19 +5,33 @@
 
 int main ()
 {
-	int choix = 0;
-	int choix2 = 0;
+	/*int choix1 = 0;
+	int choix2 = 0;*/
 	
-	printf("voulez vous aller au mordor  1 ? ou dans la comptee 2 ?");
-	scanf("%d",&choix);
+	char mordor[40];
+	char comptee [40];
 	
-	if (choix == 1){
+	printf("voulez vous aller au mordor ?");
+	scanf("%s", mordor );
+	
+	printf("voulez vous aller a la comptee ?");
+	scanf("%s", comptee );
+	
+	if ( strcmp( mordor, "mordo" ) == 0) {
+        printf( "vous etes arrive au mordor\n" );
+	}
+	
+	if ( strcmp( comptee, "compt" ) == 0) {
+        printf( "vous etes arrive dans la comptee\n" );
+	}
+	
+	/*if (choix == 1){
 		printf("vous allez au mordor");
 	}
 	
 	if (choix2 == 1){
 		printf("vous allez a la comptee");
-	}
+	}*/
 	
 	/*if (strcmp("pd")){
 		
